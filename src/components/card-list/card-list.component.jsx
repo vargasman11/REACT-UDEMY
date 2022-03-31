b6a7adee84ff93c6b6d99d9d0c7b756d34f13fbe
Jerry Vargas <vargasman11@gmail.com>
2022-03-30 20:42:40 -0500

    adding finishing touches

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,7 @@
 import {Component} from "react";
 
+import Card from '../card/card.component';
+import './card-list.styles.css';
 class CardList extends Component {
 
     render() {
@@ -8,15 +10,8 @@ class CardList extends Component {
         return (
             <div className="card-list">
                 {monsters.map(monster => {
-                    const {name, email, id} = monster;
-                return (
-                        <div key={id} className="card-container">
-                            <img src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                                 alt={`${name}`}/>
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
-                    )})}
+                    return  <Card key={monster.id} monster={monster} />
+                })}
             </div>
         )
     }
